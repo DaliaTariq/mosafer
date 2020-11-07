df30ea80d1e16d94bf42a18f3e8a64135c23e12f
Dalia Tariq <daliatariq95@gmail.com>
2020-11-07 12:27:15 +0300

    add file

---
+++ js/main.js
@@ -0,0 +1,60 @@
+$(document).ready(function () {
+    //Top Nav (Scroling Navbar)//
+    scrollShow = false;
+    $(window).scroll(function () {
+        scrollTop = $(window).scrollTop();
+
+        if (scrollTop > 0) {
+            if (scrollShow)
+                return;
+            console.log(scrollShow);
+            scrollShow = true;
+            $('.navbar').addClass('scrollNav');
+            $('.navbar').css('opacity', 0)
+                .slideDown('slow').animate({ opacity: 1 },
+                    { queue: false, duration: 'slow' }
+                );
+        }
+        else {
+            $('.navbar').removeClass('scrollNav');
+            /*console.log('false');*/
+            scrollShow = false;
+        }
+    });
+
+
+
+
+    /**Call Carousel*/
+    $('.categOwl').owlCarousel({
+        loop: true,
+        autoplay: true,
+        dots: false,
+        margin: 10,
+        autoplayHoverPause: true,
+        smartSpeed: 350,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: true
+            },
+            575: {
+                items: 2,
+                nav: true
+            },
+
+            800: {
+                items: 2,
+                nav: true
+            },
+            1025: {
+                items: 5,
+                nav: true,
+                loop: true
+            }
+        }
+    });
+    
+
+});
